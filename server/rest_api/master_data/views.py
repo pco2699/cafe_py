dda7e182d9d4a3199640f8f65c54ab47f1dcb3a9
pco2699 <aenkun@gmail.com>
2017-09-12 01:02:52 +0900

    hoge

--- server/rest_api/master_data/views.py
+++ server/rest_api/master_data/views.py
@@ -17,6 +17,13 @@ class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
 
+class EnvriromentFilter(filters.FilterSet):
+    min_time = django_filters.DateTimeFilter(name="time", lookup_expr='gte')
+    max_time = django_filters.DateTimeFilter(name="time", lookup_expr='lte')
+
+    class Meta:
+        model = Environment
+        fields = ['place', 'min_time', 'max_time']
 
 class EnvironmentViewSet(viewsets.ModelViewSet):
     queryset = Environment.objects.all()
